f59f4b93bac478a15d1b67b5fa242ef8c99a0312
jakeorr <jake.a.orr@gmail.com>
2021-09-20 11:54:02 -0700

    simplfy to only dynamic js file

--- pages/a-funnel/first-page.js
+++ pages/a-funnel/first-page.js
@@ -16,13 +16,8 @@ export default function Home() {
           First Page
         </h1>
 
-        <h2>Orders page usees dynamic segment has a directory</h2>
-        <Link href="/orders/1234asdf?fid=a-funnel"><a>Order Page (will 404 after refresh of the page)</a></Link>
-        <a href="/orders/1234asdf?fid=a-funnel">Order Page (regular link - will 404 immediately)</a>
-        <br />
-        <h2>Another page uses dynamic segment as js file</h2>
-        <Link href="/another/1234asdf?fid=a-funnel"><a>Another Page (will 404 after refresh of the page)</a></Link>
-        <a href="/another/1234asdf?fid=a-funnel">Another Page (regular link - will 404 immediately)</a>
+        <Link href="/orders/1234asdf?fid=a-funnel"><a>Order Page (next/link: will 404 after refresh of the page)</a></Link>
+        <a href="/orders/1234asdf?fid=a-funnel">Order Page (regular link: will 404 immediately)</a>
       </main>
     </div>
   )

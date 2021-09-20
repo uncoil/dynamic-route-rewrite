e48568d2733a48129af68840bbbc5ca7e7cc1e15
jakeorr <jake.a.orr@gmail.com>
2021-09-20 11:21:37 -0700

    correct path

--- pages/a-funnel/orders/[orderId]/index.js
+++ pages/a-funnel/orders/[orderId]/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import { useRouter } from 'next/router'
-import styles from '../../../styles/Home.module.css'
+import styles from '../../../../styles/Home.module.css'
 
 export default function Home() {
   const { query } = useRouter();
